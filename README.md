c520cacef48ef282dc8961c8b41a2bde8a49efbe
wszdwp <wszdwp@gmail.com>
2015-05-02 09:45:34 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#AlgorithmPractice
+
+##LeetCode
---
+++ test/TestCases.java
@@ -0,0 +1,18 @@
+
+public class TestCases {
+	//Arrays
+	//Integer
+	int[] num0 = {1, 2};
+	int[] num1 = {2, 1};
+	int[] num2 = {1, 2, 3};
+	int[] num3 = {3, 2, 1};
+	int[] num4 = {-1, 1, 1, 1, 2, 1};
+	int[] num5 = {-32, 2 , 4, 6, 8, 10, -2 , 5, 0, 31, 1, -3, -1, -5, -1};
+ 	int[] num6 = {0, 0, 0, 0};
+ 	int[] num7 = {-2,0,1,1,2};
+ 	
+ 	//String
+ 	String[] s0 = {"()()", "()[]{}", "()"};
+	String[] s1 = {"([)]", "(]"};
+
+}
