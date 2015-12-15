2607ae7811479e3c675038b5226e1d2fc0b41fab
wszdwp <wszdwp@gmail.com>
2015-12-14 23:16:22 -0500

    DuplivateArrayElem solu2

--- src/com/codingpan/leetcode/passedOJ/DuplicateArrayElem.java
+++ src/com/codingpan/leetcode/passedOJ/DuplicateArrayElem.java
@@ -48,6 +48,38 @@ public class DuplicateArrayElem {
         return isDuplicate;
     }
 	
+	public static boolean containsDuplicate2(int[] nums) {
+        boolean isDuplicate = false;
+        int len = nums.length;
+        
+        if (len < 2) {	return false; }
+
+        BitSet myPosBitSet = new BitSet();
+        BitSet myNegBitSet = new BitSet();
+        for (int i = 0; i < len; i++) {
+        	if (nums[i] == Integer.MIN_VALUE)	continue;
+        	if (nums[i] < 0)	{
+        		int bIndex = -nums[i];
+        		if (myNegBitSet.get(bIndex)) {
+            		isDuplicate = true;
+            		return isDuplicate;
+            	} else {
+            		myNegBitSet.set(bIndex);
+            	}
+        	} else {
+        		int bIndex = nums[i];
+        		if (myPosBitSet.get(bIndex)) {
+            		isDuplicate = true;
+            		return isDuplicate;
+            	} else {
+            		myPosBitSet.set(bIndex);
+            	}
+        	}
+        }
+        
+        return isDuplicate;
+    }
+	
 	public static void main(String[] args) {
 		int[] nums1 = {9, 4, 35, 1920, 214, 4};
 		int[] nums2 = {9, 4, 35, 1920, 214, 425};
@@ -60,6 +92,12 @@ public class DuplicateArrayElem {
 		System.out.println("Expected(False): " + (containsDuplicate(nums3)?"True":"False"));
 		System.out.println("Expected(True): " + (containsDuplicate(nums4)?"True":"False"));
 		System.out.println("Expected(False): " + (containsDuplicate(nums5)?"True":"False"));
+		
+		System.out.println("Expected(True): " + (containsDuplicate2(nums1)?"True":"False"));
+		System.out.println("Expected(False): " + (containsDuplicate2(nums2)?"True":"False"));
+		System.out.println("Expected(False): " + (containsDuplicate2(nums3)?"True":"False"));
+		System.out.println("Expected(True): " + (containsDuplicate2(nums4)?"True":"False"));
+		System.out.println("Expected(False): " + (containsDuplicate2(nums5)?"True":"False"));
 
 		
 	}
